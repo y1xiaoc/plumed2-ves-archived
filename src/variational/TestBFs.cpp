a33e338eede2894b6b3276a0f8260a7a849ce913
Omar Valsson <omar.valsson@gmail.com>
2015-04-13 11:53:03 +0200

    Added action to test 1D basis sets

---
+++ src/variational/TestBFs.cpp
@@ -0,0 +1,105 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Copyright (c) 2014 The plumed team
+   (see the PEOPLE file at the root of the distribution for a list of names)
+
+   See http://www.plumed-code.org for more information.
+
+   This file is part of plumed, version 2.
+
+   plumed is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   plumed is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#include "../function/Function.h"
+#include "core/ActionRegister.h"
+#include "core/ActionSet.h"
+#include "core/PlumedMain.h"
+#include "BasisFunctions.h"
+
+using namespace std;
+
+namespace PLMD{
+namespace function{
+
+//+PLUMEDOC FUNCTION TEST_BASISFUNCTIONS
+/*
+
+*/
+//+ENDPLUMEDOC
+
+
+class TestBFs :
+  public Function
+{
+  BasisFunctions::BasisFunctions* bf_pointer;
+  unsigned int bf_order_;
+public:
+  TestBFs(const ActionOptions&);
+  void calculate();
+  static void registerKeywords(Keywords& keys);
+};
+
+
+PLUMED_REGISTER_ACTION(TestBFs,"TEST_BASISFUNCTIONS")
+
+void TestBFs::registerKeywords(Keywords& keys){
+  Function::registerKeywords(keys);
+  keys.addOutputComponent("value","default","value of basis function");
+  keys.addOutputComponent("deriv","default","deriative of basis function");
+  keys.addOutputComponent("arg","default","input argument");
+  keys.addOutputComponent("argT","default","translated input argument");
+  keys.addOutputComponent("inside","default","1.0 if inside interval, otherwise 0.0");
+  keys.use("ARG");
+  keys.add("compulsory","BASIS_SET","the label of the basis set that you want to use");
+  keys.add("compulsory","N","the number of the basis function you want to test");
+}
+
+TestBFs::TestBFs(const ActionOptions&ao):
+Action(ao),
+Function(ao)
+{
+  if(getNumberOfArguments()>1){error("only one argument allowed");}
+  std::string basisset_label="";
+  parse("BASIS_SET",basisset_label);
+  bf_pointer=plumed.getActionSet().selectWithLabel<BasisFunctions::BasisFunctions*>(basisset_label);
+  // bf_pointer->printInfo();
+  parse("N",bf_order_);
+  addComponent("value"); componentIsNotPeriodic("value");
+  addComponent("deriv"); componentIsNotPeriodic("deriv");
+  addComponent("arg"); componentIsNotPeriodic("arg");
+  addComponent("argT"); componentIsNotPeriodic("argT");
+  addComponent("inside"); componentIsNotPeriodic("inside");
+  checkRead();
+  log.printf("  using the %d order basis function from the %s basis set\n",bf_order_,basisset_label.c_str());
+}
+
+void TestBFs::calculate(){
+  
+  std::vector<double> values(bf_pointer->getNumberOfBasisFunctions());
+  std::vector<double> derivs(bf_pointer->getNumberOfBasisFunctions());
+  bool inside_interval=true;
+  double arg=getArgument(0);
+  double argT=0.0;
+  bf_pointer->getAllValues(arg,argT,inside_interval,values,derivs);
+
+  getPntrToComponent("value")->set(values[bf_order_]);
+  getPntrToComponent("deriv")->set(derivs[bf_order_]);
+  getPntrToComponent("argT")->set(argT);
+  getPntrToComponent("arg")->set(arg);
+  if(inside_interval){getPntrToComponent("inside")->set(1.0);}
+  else{getPntrToComponent("inside")->set(0.0);}
+}
+
+}
+}
+
+
