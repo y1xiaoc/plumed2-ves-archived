ff29d9b179e5001a0a5000ca916b445be30d2007
Omar Valsson <omar.valsson@gmail.com>
2015-05-20 14:55:58 +0200

    Addad a abstract base class for biases that can include functions specfic to the variational approach

---
+++ src/variational/VariationalBias.h
@@ -0,0 +1,53 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Copyright (c) 2011-2014 The plumed team
+   (see the PEOPLE file at the root of the distribution for a list of names)
+
+   See http://www.plumed-code.org for more information.
+
+   This file is part of plumed, version 2.
+
+   plumed is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   plumed is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#ifndef __PLUMED_variational_VariationalBias_h
+#define __PLUMED_variational_VariationalBias_h
+
+#include "core/ActionPilot.h"
+#include "core/ActionWithValue.h"
+#include "core/ActionWithArguments.h"
+#include "bias/Bias.h"
+
+#define PLUMED_VARIATIONALBIAS_INIT(ao) Action(ao),VariationalBias(ao)
+
+namespace PLMD{
+namespace bias{
+
+/**
+\ingroup INHERIT
+Abstract base class for implementing biases the extents the normal Bias.h class 
+to include functions related to the variational approach.
+*/
+
+class VariationalBias:
+  public Bias
+{
+public:
+  static void registerKeywords(Keywords&);
+  VariationalBias(const ActionOptions&ao);
+};
+
+}
+}
+
+#endif
+
