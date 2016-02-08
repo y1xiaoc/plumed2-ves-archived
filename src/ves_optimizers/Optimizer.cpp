452f4f8ed50950cc0c59bdf12ed90a5676710adf
Omar Valsson <omar.valsson@gmail.com>
2016-02-08 17:04:39 +0100

    Optimizer: better error msg

--- src/ves_optimizers/Optimizer.cpp
+++ src/ves_optimizers/Optimizer.cpp
@@ -204,7 +204,7 @@ identical_coeffs_shape_(true)
     }
     parse("TARGETDISTRIBUTION_STRIDE",ustride_targetdist_);
     if(need_stride && ustride_targetdist_==0){
-      plumed_merror("you need to give stride for updating a target distribution by using the TARGETDISTRIBUTION_STRIDE keyword");
+      plumed_merror("one of the biases has a dynamic target distribution so you need to give stride for updating it by using the TARGETDISTRIBUTION_STRIDE keyword");
     }
     if(!need_stride && ustride_targetdist_!=0){
       plumed_merror("using the TARGETDISTRIBUTION_STRIDE keyword doesn't make sense as there is no target distribution to update");
