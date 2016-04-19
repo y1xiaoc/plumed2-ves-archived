c32a8c4187f09066221cc42e54c75b8fe1120eaa
Omar Valsson <omar.valsson@gmail.com>
2016-04-19 20:21:15 +0200

    Changed the header on the VES file

--- src/ves_tools/CoeffsMatrix.cpp
+++ src/ves_tools/CoeffsMatrix.cpp
@@ -1,23 +1,23 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-   Copyright (c) 2011-2014 The plumed team
-   (see the PEOPLE file at the root of the distribution for a list of names)
+   Copyright (c) 2015-2016 The ves-code team
+   (see the PEOPLE-VES file at the root of the distribution for a list of names)
 
-   See http://www.plumed-code.org for more information.
+   See http://www.ves-code.org for more information.
 
-   This file is part of plumed, version 2.
+   This file is part of ves-code, version 1.
 
-   plumed is free software: you can redistribute it and/or modify
+   ves-code is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   plumed is distributed in the hope that it will be useful,
+   ves-code is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public License
-   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
+   along with ves-code.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 #include <vector>
 #include <cmath>
