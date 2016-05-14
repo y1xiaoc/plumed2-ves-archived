c090e7a25c5bda18f3e16241c385965e88c33622
Omar Valsson <omar.valsson@gmail.com>
2016-05-14 16:43:51 +0200

    Initial version of the S2 methyl contact model CV

---
+++ src/function/S2MethylContactModel.cpp
@@ -0,0 +1,118 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Copyright (c) 2011-2016 The plumed team
+   (see the PEOPLE file at the root of the distribution for a list of names)
+
+   See http://www.plumed.org for more information.
+
+   This file is part of plumed, version 2.
+
+   plumed is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   plumed is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#include "Function.h"
+#include "ActionRegister.h"
+
+#include <cmath>
+
+using namespace std;
+
+namespace PLMD{
+namespace function{
+
+//+PLUMEDOC FUNCTION S2_METHYL_CONTACT_MODEL
+/*
+
+
+
+
+*/
+//+ENDPLUMEDOC
+
+
+class S2MethylContactModel :
+  public Function
+{
+  double r_eff_;
+  double inv_r_eff_;
+  double prefactor_a_;
+  double exp_b_;
+  double offset_c_;
+  double n_i_;
+  double total_prefactor_;
+public:
+  explicit S2MethylContactModel(const ActionOptions&);
+  void calculate();
+  static void registerKeywords(Keywords& keys);
+};
+
+
+PLUMED_REGISTER_ACTION(S2MethylContactModel,"S2_METHYL_CONTACT_MODEL")
+
+void S2MethylContactModel::registerKeywords(Keywords& keys){
+  Function::registerKeywords(keys);
+  keys.use("ARG");
+  keys.add("compulsory","R_EFF","the effective distance, r_eff in the equation, given in nm.");
+  keys.add("compulsory","PREFACTOR_A","the prefactor, a in the equation");
+  keys.add("compulsory","EXPONENT_B","the exponent, b in the equation");
+  keys.add("compulsory","OFFSET_C","the offset, c in the equation");
+  keys.add("compulsory","N_I"," n_i in the equation");
+}
+
+S2MethylContactModel::S2MethylContactModel(const ActionOptions&ao):
+Action(ao),
+Function(ao),
+r_eff_(0.0),
+inv_r_eff_(0.0),
+prefactor_a_(0.0),
+exp_b_(0.0),
+offset_c_(0.0),
+n_i_(0.0),
+total_prefactor_(0.0)
+{
+  parse("R_EFF",r_eff_);
+  inv_r_eff_ = 1.0/r_eff_;
+  parse("PREFACTOR_A",prefactor_a_);
+  parse("EXPONENT_B",exp_b_);
+  parse("OFFSET_C",offset_c_);
+  unsigned int n_i_int;
+  parse("N_I",n_i_int);
+  n_i_ = static_cast<double>(n_i_int);
+  total_prefactor_ = prefactor_a_/pow(n_i_,exp_b_);
+
+  addValueWithDerivatives();
+  setNotPeriodic();
+  checkRead();
+
+}
+
+void S2MethylContactModel::calculate(){
+
+  double contact_sum = 0.0;
+  std::vector<double> exp_arg(getNumberOfArguments());
+  for(unsigned int i=0; i<getNumberOfArguments(); i++){
+    exp_arg[i] = exp(-getArgument(i)*inv_r_eff_);
+    contact_sum += exp_arg[i];
+  }
+
+  double value = tanh(total_prefactor_*contact_sum);
+  // using that d/dx[tanh(x)]= 1-[tanh(x)]^2
+  double deriv_f = -inv_r_eff_*total_prefactor_*(1.0-value*value);
+  value -= offset_c_;
+  setValue(value);
+  for(unsigned int i=0; i<getNumberOfArguments(); i++){
+    setDerivative(i,deriv_f*exp_arg[i]);
+  }
+}
+
+}
+}
