83e1a2c837f666ec32c759a6111ff6e52e3ef26c
Omar Valsson <omar.valsson@gmail.com>
2016-05-30 14:08:41 +0200

    Added stochastic gradient decent

---
+++ src/ves_optimizers/StochasticGradientDescent.cpp
@@ -0,0 +1,80 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Copyright (c) 2015-2016 The ves-code team
+   (see the PEOPLE-VES file at the root of the distribution for a list of names)
+
+   See http://www.ves-code.org for more information.
+
+   This file is part of ves-code, version 1.
+
+   ves-code is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ves-code is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with ves-code.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#include "Optimizer.h"
+#include "ves_tools/CoeffsVector.h"
+
+#include "core/ActionRegister.h"
+
+
+namespace PLMD{
+
+class StochasticGradientDescent : public Optimizer {
+private:
+  double decay_constant_;
+public:
+  static void registerKeywords(Keywords&);
+  explicit StochasticGradientDescent(const ActionOptions&);
+  void coeffsUpdate(const unsigned int c_id = 0);
+};
+
+
+PLUMED_REGISTER_ACTION(StochasticGradientDescent,"STOCHASTIC_GRADIENT_DESCENT")
+
+
+void StochasticGradientDescent::registerKeywords(Keywords& keys){
+  Optimizer::registerKeywords(keys);
+  Optimizer::useDynamicStepSizeKeywords(keys);
+  Optimizer::useMultipleWalkersKeywords(keys);
+  Optimizer::useMaskKeywords(keys);
+  Optimizer::useRestartKeywords(keys);
+  Optimizer::useMonitorAveragesKeywords(keys);
+  Optimizer::useDynamicTargetDistributionKeywords(keys);
+  keys.add("optional","DECAY_CONSTANT","the decay constant used for the step size.");
+  }
+
+
+StochasticGradientDescent::StochasticGradientDescent(const ActionOptions&ao):
+PLUMED_OPTIMIZER_INIT(ao),
+decay_constant_(1.0)
+{
+  parse("DECAY_CONSTANT",decay_constant_);
+  if(decay_constant_<1.0){
+    plumed_merror("the value given in DECAY_CONSTANT doesn't make sense, it should be larger than 1.0");
+  }
+  if(decay_constant_>1.0){
+    log.printf("  using a decay constant of %f\n",decay_constant_);
+  }
+  checkRead();
+}
+
+
+void StochasticGradientDescent::coeffsUpdate(const unsigned int c_id) {
+  double current_stepsize =  StepSize(c_id) /(1.0 + getIterationCounterDbl()/decay_constant_);
+  setCurrentStepSize(current_stepsize,c_id);
+  Coeffs(c_id) = Coeffs(c_id) - current_stepsize * CoeffsMask(c_id) * Gradient(c_id);
+  //
+  double aver_decay = 1.0 / ( getIterationCounterDbl() + 1.0 );
+  AuxCoeffs(c_id) += aver_decay * ( Coeffs(c_id)-AuxCoeffs(c_id) );
+}
+
+
+}
