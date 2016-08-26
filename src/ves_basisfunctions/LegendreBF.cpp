dcbfc5014d59fbe90f59914be53a0b3c3f1978a7
Omar Valsson <omar.valsson@gmail.com>
2016-08-26 14:16:26 +0200

    Added Legendre basis functions

---
+++ src/ves_basisfunctions/LegendreBF.cpp
@@ -0,0 +1,98 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Copyright (c) 2015-2016 The ves-code team
+   (see the PEOPLE-VES file at the root of the distribution for a list of names)
+
+   See http://www.ves-code.org for more information.
+
+   This file is part of ves-code, version 1.
+
+   ves-code is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ves-code is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with ves-code.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#include "BasisFunctions.h"
+
+#include "core/ActionRegister.h"
+
+namespace PLMD{
+
+class LegendreBF : public BasisFunctions {
+  virtual void setupUniformIntegrals();
+public:
+  static void registerKeywords(Keywords&);
+  explicit LegendreBF(const ActionOptions&);
+  double getValue(const double, const unsigned int, double&, bool&) const;
+  void getAllValues(const double, double&, bool&, std::vector<double>&, std::vector<double>&) const;
+};
+
+
+PLUMED_REGISTER_ACTION(LegendreBF,"BF_LEGENDRE")
+
+
+void LegendreBF::registerKeywords(Keywords& keys){
+ BasisFunctions::registerKeywords(keys);
+}
+
+LegendreBF::LegendreBF(const ActionOptions&ao):
+ PLUMED_BASISFUNCTIONS_INIT(ao)
+{
+  setNumberOfBasisFunctions(getOrder()+1);
+  setIntrinsicInterval("-1.0","+1.0");
+  setNonPeriodic();
+  setIntervalBounded();
+  setType("Legendre");
+  setDescription("Legendre polynomials of the first kind");
+  setLabelPrefix("P");
+  setupBF();
+}
+
+
+double LegendreBF::getValue(const double arg, const unsigned int n, double& argT, bool& inside_range) const {
+  plumed_massert(n<numberOfBasisFunctions(),"getValue: n is outside range of the defined order of the basis set");
+  inside_range=true;
+  std::vector<double> tmp_values(numberOfBasisFunctions());
+  std::vector<double> tmp_derivs(numberOfBasisFunctions());
+  getAllValues(arg, argT, inside_range, tmp_values, tmp_derivs);
+  return tmp_values[n];
+}
+
+
+void LegendreBF::getAllValues(const double arg, double& argT, bool& inside_range, std::vector<double>& values, std::vector<double>& derivs) const {
+  // plumed_assert(values.size()==numberOfBasisFunctions());
+  // plumed_assert(derivs.size()==numberOfBasisFunctions());
+  inside_range=true;
+  argT=translateArgument(arg, inside_range);
+  std::vector<double> derivsT(derivs.size());
+  //
+  values[0]=1.0;
+  derivsT[0]=0.0;
+  derivs[0]=0.0;
+  values[1]=argT;
+  derivsT[1]=1.0;
+  derivs[1]=intervalDerivf();
+  for(unsigned int i=1; i < getOrder();i++){
+    double io = static_cast<double>(i);
+    values[i+1]  = ((2.0*io+1.0)/(io+1.0))*argT*values[i] - (io/(io+1.0))*values[i-1];
+    derivsT[i+1] = ((2.0*io+1.0)/(io+1.0))*(values[i]+argT*derivsT[i])-(io/(io+1.0))*derivsT[i-1];
+    derivs[i+1]  = intervalDerivf()*derivsT[i+1];
+  }
+  if(!inside_range){for(unsigned int i=0;i<derivs.size();i++){derivs[i]=0.0;}}
+}
+
+
+void LegendreBF::setupUniformIntegrals() {
+  setAllUniformIntegralsToZero();
+  setUniformIntegral(0,1.0);
+}
+
+
+}
