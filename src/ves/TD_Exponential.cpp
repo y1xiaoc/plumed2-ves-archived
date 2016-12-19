dce0b6b5ad0ffef31209598dfd1e89037d078ff1
Omar Valsson <omar.valsson@gmail.com>
2016-12-19 19:49:18 +0100

    Exponential target dist

---
+++ src/ves/TD_Exponential.cpp
@@ -0,0 +1,96 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Copyright (c) 2015-2016 The ves-code team
+   (see the PEOPLE-VES file at the root of the distribution for a list of names)
+
+   See http://www.ves-code.org for more information.
+
+   This file is part of ves-code, version 1.
+
+   ves-code is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ves-code is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with ves-code.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+#include "TargetDistribution.h"
+#include "TargetDistributionRegister.h"
+
+#include "tools/Keywords.h"
+
+
+namespace PLMD{
+namespace ves{
+
+//+PLUMEDOC VES_TARGETDIST EXPONENTIAL
+/*
+Exponential distribution (static).
+
+\par Examples
+
+*/
+//+ENDPLUMEDOC
+
+class TD_Exponential: public TargetDistribution {
+  std::vector<double> minima_;
+  std::vector<double> lambda_;
+  double ExponentialDiagonal(const std::vector<double>&, const std::vector<double>&, const std::vector<double>&) const;
+public:
+  static void registerKeywords(Keywords&);
+  explicit TD_Exponential(const TargetDistributionOptions& to);
+  double getValue(const std::vector<double>&) const;
+};
+
+
+VES_REGISTER_TARGET_DISTRIBUTION(TD_Exponential,"EXPONENTIAL")
+
+
+void TD_Exponential::registerKeywords(Keywords& keys){
+  TargetDistribution::registerKeywords(keys);
+  keys.add("compulsory","MINIMA","The minima of the exponential distribution.");
+  keys.add("compulsory","LAMBDA","The lambda parameters for the chi-squared distribution.");
+}
+
+
+TD_Exponential::TD_Exponential( const TargetDistributionOptions& to ):
+TargetDistribution(to),
+minima_(0),
+lambda_(0)
+{
+  parseVector("MINIMA",minima_);
+  parseVector("LAMBDA",lambda_);
+
+
+  setDimension(minima_.size());
+  if(lambda_.size()!=getDimension()){plumed_merror(getName()+": the LAMBDA keyword does not match the given dimension in MINIMA");}
+  checkRead();
+}
+
+
+double TD_Exponential::getValue(const std::vector<double>& argument) const {
+  for(unsigned int k=0; k<argument.size(); k++){
+    if(argument[k]<minima_[k]){plumed_merror(getName()+": the exponential distribution is not defined for values less that ones given in MINIMA");}
+  }
+  return ExponentialDiagonal(argument,minima_,lambda_);
+}
+
+
+double TD_Exponential::ExponentialDiagonal(const std::vector<double>& argument, const std::vector<double>& minima, const std::vector<double>& lambda) const {
+  double value = 1.0;
+  for(unsigned int k=0; k<argument.size(); k++){
+    value *= lambda[k]*exp(-(argument[k]-minima[k])*lambda[k]);
+  }
+  return value;
+}
+
+
+
+}
+}
