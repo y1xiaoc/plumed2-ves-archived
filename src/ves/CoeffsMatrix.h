a580a2806a820116cabce691973bf49fef75de31
Omar Valsson <omar.valsson@gmail.com>
2017-03-21 12:10:13 +0100

    Run header script on ves

--- src/ves/CoeffsMatrix.h
+++ src/ves/CoeffsMatrix.h
@@ -19,7 +19,6 @@
    You should have received a copy of the GNU Lesser General Public License
    along with ves-code.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-
 #ifndef __PLUMED_ves_CoeffsMatrix_h
 #define __PLUMED_ves_CoeffsMatrix_h
 
