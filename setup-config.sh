c1e414cc5080d0bb9b0e781f4b6fcae062a04e05
Omar Valsson <omar.valsson@gmail.com>
2017-06-28 11:12:18 +0200

    Removed setup-config.sh added my mistake

--- setup-config.sh
+++
@@ -1,3 +0,0 @@
-export LDFLAGS="-L/Users/valsson/programs/libmatheval-1.1.11/lib -L/Users/valsson/programs/xdrfile-1.1.4/lib"
-export CPPFLAGS="-I/Users/valsson/programs/libmatheval-1.1.11/include -I//Users/valsson/programs/xdrfile-1.1.4/include"
-./configure  --enable-matheval --enable-modules=all
