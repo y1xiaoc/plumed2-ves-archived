8968061300439ea51a1489a483b75baf79c124b0
Omar Valsson <omar.valsson@gmail.com>
2017-08-17 22:33:25 +0200

    Removed hasDeriv... - will be readded in next commit

--- src/tools/Grid.h
+++ src/tools/Grid.h
@@ -127,8 +127,6 @@ public:
   unsigned getDimension() const;
 /// get argument names  of this grid
   std::vector<std::string> getArgNames() const;
-/// get if the grid has derivatives
-  bool hasDerivatives() const {return usederiv_;}
 
 /// methods to handle grid indices
   std::vector<unsigned> getIndices(index_t index) const;
