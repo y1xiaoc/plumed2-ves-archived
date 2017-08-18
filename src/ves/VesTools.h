6e19ecadb80ef88414fc754b26643867dbc39c12
Omar Valsson <omar.valsson@gmail.com>
2017-08-18 15:29:20 +0200

    Better functions for get Action pointers

--- src/ves/VesTools.h
+++ src/ves/VesTools.h
@@ -28,6 +28,8 @@
 #include <limits>
 #include <vector>
 
+#include "core/ActionSet.h"
+
 
 namespace PLMD {
 
@@ -43,6 +45,9 @@ public:
   // copy grid values
   static void copyGridValues(Grid* grid_pntr_orig, Grid* grid_pntr_copy);
   static unsigned int getGridFileInfo(const std::string&, std::string&, std::vector<std::string>&, std::vector<std::string>&, std::vector<std::string>&, std::vector<bool>&, std::vector<unsigned int>&, bool&);
+  //
+  template<typename T> static std::vector<std::string> getLabelsOfAvailableActions(const ActionSet&);
+  template<typename T> static T getPointerFromLabel(const std::string&, const ActionSet&, std::string&);
 };
 
 inline
@@ -57,6 +62,37 @@ inline
 void VesTools::convertDbl2Str(const double value,std::string& str) {
   unsigned int precision = std::numeric_limits<double>::digits10 + 1;
   convertDbl2Str(value,str,precision);
+}
+
+
+template<typename T>
+std::vector<std::string> VesTools::getLabelsOfAvailableActions(const ActionSet& actionset) {
+  std::vector<std::string> avail_action_str(0);
+  std::vector<T> avail_action_pntrs = actionset.select<T>();
+  for(unsigned int i=0; i<avail_action_pntrs.size(); i++) {
+    avail_action_str.push_back(avail_action_pntrs[i]->getLabel());
+  }
+  return avail_action_str;
+}
+
+template<typename T>
+T VesTools::getPointerFromLabel(const std::string& action_label, const ActionSet& actionset, std::string& error_msg) {
+  T action_pntr = actionset.selectWithLabel<T>(action_label);
+  error_msg = "";
+  if(action_pntr==NULL) {
+    error_msg = "label "+action_label+" does not exist\n"; 
+    std::vector<T> avail_action_pntrs = actionset.select<T>();
+    if(avail_action_pntrs.size()>0) {
+      error_msg += "             Hint! the actions defined in the input file that can be used here are: \n";
+      for(unsigned int i=0; i<avail_action_pntrs.size(); i++) {
+        error_msg += "             " + avail_action_pntrs[i]->getName() + " with label " + avail_action_pntrs[i]->getLabel() + "\n";
+      }    
+    }
+    else {
+      error_msg += "             Hint! no actions defined in the input file that can be used here, they should be defined before this actions\n";
+    }
+  }
+  return action_pntr;
 }
 
 
