952561177c778e69908e1bc7acfb2a9a7b9b5a66
Yixiao Chen <chenyix@mendrisio.usilu.net>
2017-09-08 18:08:48 +0200

    change MXHOME

--- before_make.sh
+++ before_make.sh
@@ -1,4 +1,4 @@
-MX_HOME=/home/chenyix/mxnet
+MX_HOME=$HOME/mxnet
 export CPATH=$MX_HOME/include:$MX_HOME/nnvm/include:$MX_HOME/dmlc-core/include:$MX_HOME/cpp-package/include:$CPATH
 export LD_LIBRARY_PATH=$MX_HOME/lib:$LD_LIBRARY_PATH
 export LIBRARY_PATH=$MX_HOME/lib:$LIBRARY_PATH
